340bd1bc58517bcf0e060e280f71300836c8c268
Manukakalmitha <manukakalmitha@gmail.com>
2026-01-14 16:47:07 +0530

    major update4

---
+++ diverai-twa/app/src/main/java/agency/flisoft/diverai/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package agency.flisoft.diverai.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
